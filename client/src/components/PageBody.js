e48c656374767740d3648730fa61c886b34b6d41
Sumbal Latif <sumballatif98@gmail.com>
2022-03-02 14:38:55 +0500

    change gradient colors

--- client/src/components/PageBody.js
+++ client/src/components/PageBody.js
@@ -9,7 +9,7 @@ export function PageBody() {
         style={{
           background:
             // "linear-gradient(145deg, #CCD9D7, #F0F8F7 10%, #CCD9D7 90%, #F0F8F7)",
-            "linear-gradient(30deg, #D8E7E5 4%, #F0F8F7 7%, #F0F8F7 60%, #CCD9D7, #F0F8F7)",
+            "linear-gradient(130deg, #D8E7E5 4%, #F0F8F7 7%, #F0F8F7 60%, #CCD9D7, #F0F8F7)",
         }}
       >
         <CaloriesIntakeDetails />
